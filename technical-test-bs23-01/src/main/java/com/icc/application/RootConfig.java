e731a45f18acc5f28f9260598a35cbd6929b1929
Istiaq Hossain <hossain.ict@gmail.com>
2020-02-15 17:48:55 +0600

    feat: -add WebSecurityConfiguration -add basePackages(service,persistence,security) in RootConfig

--- technical-test-bs23-01/src/main/java/com/icc/application/RootConfig.java
+++ technical-test-bs23-01/src/main/java/com/icc/application/RootConfig.java
@@ -2,10 +2,14 @@ package com.icc.application;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @ComponentScan(basePackages = {
-		"com.icc.applicaion.service"
+		"com.icc.applicaion.service",
+		"com.icc.applicaion.config.persistence",
+		"com.icc.applicaion.config.security",
 })
 //@ComponentScan(basePackageClasses = {StudentService.class})
 public class RootConfig {
@@ -14,5 +18,8 @@ public class RootConfig {
 		return new GlobalExceptionHandler();
 	}
 
-
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
