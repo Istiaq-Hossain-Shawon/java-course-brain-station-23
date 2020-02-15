0a296615abc8da0eed558cee62bd47816de661b2
Istiaq Hossain <hossain.ict@gmail.com>
2020-02-15 17:22:50 +0600

    feat: add course model

---
+++ technical-test-bs23-01/src/main/java/com/icc/application/model/Team.java
@@ -0,0 +1,62 @@
+package com.icc.application.model;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tbl_team")
+public class Team implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name = "id")
+	private long id;
+	@Column
+	private String name;
+	
+	@ManyToMany(fetch = FetchType.LAZY, targetEntity = User.class)
+	@JoinTable(name = "team_member", joinColumns = @JoinColumn(name = "team_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"))
+	private Set<User> users;
+	
+	@ManyToOne
+	private Country country;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}	
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	public Team() {
+	}
+}
