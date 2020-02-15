0e31159c9cb82f1780c45895c08242d3d9229e74
Istiaq Hossain <hossain.ict@gmail.com>
2020-02-15 17:13:02 +0600

    feat:add country model

---
+++ technical-test-bs23-01/src/main/java/com/icc/application/model/Country.java
@@ -0,0 +1,43 @@
+package com.icc.application.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tbl_country")
+public class Country implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private long id;
+	@Column(name = "country_name", nullable = false)
+	private String countryName;
+	public Country() {
+		super();
+	}
+	public Country(long id, String countryCode, String countryName) {
+		super();
+		this.id = id;		
+		this.countryName = countryName;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+}
