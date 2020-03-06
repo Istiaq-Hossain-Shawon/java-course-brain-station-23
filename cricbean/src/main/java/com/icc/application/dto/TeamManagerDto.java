e25fa0372e88f284ab6da1674e6e1e425735907a
Istiaq Hossain <hossain.ict@gmail.com>
2020-03-06 15:35:17 +0600

    feat(dev):add TeamManagerDto

---
+++ cricbean/src/main/java/com/icc/application/dto/TeamManagerDto.java
@@ -0,0 +1,91 @@
+package com.icc.application.dto;
+
+import java.util.Date;
+import java.util.Set;
+
+import com.icc.application.model.Role;
+
+
+
+public class TeamManagerDto {
+	private long id;
+	private String username;
+	private String name;
+	private String password;
+	private String confirmPassword;
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	private String logo;
+	private long teamId;
+public long getTeamId() {
+		return teamId;
+	}
+	public void setTeamId(long teamId) {
+		this.teamId = teamId;
+	}
+public String getLogo() {
+		return logo;
+	}
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+	public Date getDob() {
+		return dob;
+	}
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+private Set<Role> roles;
+	
+    public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+	private int age;
+	private Date dob;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Date getDOB() {
+		return dob;
+	}
+	public void setDOB(Date dOB) {
+		dob = dOB;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
